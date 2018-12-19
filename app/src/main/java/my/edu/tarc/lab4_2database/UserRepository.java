77ee65692ea7861d45928ea266375bd807c44c39
simkahyong <simky-wa16@student.tarc.edu.my>
2018-12-19 18:08:33 +0800

    Initial

---
+++ app/src/main/java/my/edu/tarc/lab4_2database/UserRepository.java
@@ -0,0 +1,46 @@
+package my.edu.tarc.lab4_2database;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+import java.util.List;
+
+//TODO 6: Create a repository class to manage data query thread
+
+public class UserRepository {
+    private UserDao userDao;
+    private LiveData<List<User>> allUsers;
+
+    public UserRepository(Application application) {
+        //Get or create an instance of database
+        AppDatabase db = AppDatabase.getDatabase(application);
+        //Get an instance of DAO
+        userDao = db.userDao();
+        //Retrieve all the user records
+        allUsers = userDao.loadAllUsers();
+    }
+
+    //Feed the LiveData with all user records
+    LiveData<List<User>> getAllUsers(){
+        return allUsers;
+    }
+    
+    public void insertUser(User user){
+        new insertAsyncTask(userDao).execute(user);
+    }
+
+    //<Param, Progress, Results>
+    private static class insertAsyncTask extends AsyncTask<User, Void, Void> {
+        private UserDao userDao;
+
+        public insertAsyncTask(UserDao userDao) {
+            this.userDao = userDao;
+        }
+
+        @Override
+        protected Void doInBackground(User... users) {
+            userDao.insertUser(users[0]);
+            return null;
+        }
+    }
+}
